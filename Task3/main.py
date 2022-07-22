088f0f7d7f73f1bd0fd64c1237d8468cca3d84ad
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-07-22 23:05:17 +0530

    Update main.py

--- Task3/main.py
+++ Task3/main.py
@@ -1,1 +1,6 @@
-hello there
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemma = WordNetLemmatizer()
+print(lemma.lemmatize("cacti"))
+print(lemma.lemmatize("better",pos="a"))
+print(lemma.lemmatize("am",pos="v"))
